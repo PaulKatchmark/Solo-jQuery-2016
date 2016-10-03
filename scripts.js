ed7f42c974318eb006cce9bc95c1214c9058272a
PaulKatchmark <paulkatchmark@gmail.com>
2016-10-03 16:35:26 -0500

    a solution that actually targets my classes

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 
-//WAS FLUSTERED AND COULDNT THINK OF HOW TO TARGET CLASS IN CSS
-
+// var yellowSquare = $('yellowBlock').addClass('.color-cube', function());
 
 var clickCountRed = 0;
 var clickCountYellow = 0;
@@ -10,28 +9,36 @@ var clickCountBlue = 0;
 $(document).ready(function()  {
 
   $(".color-button[data-color='red'").on('click', function()  {
-      $(".container").append('redSquare');
+      var redSquare = $('<div></div>');
+      $(".container").append(redSquare);
+      redSquare.addClass('color-cube').addClass('red');
       $('#red').empty();
       clickCountRed +=1;
       $('#red').append('Total red: ' + clickCountRed);
   });
 
 $(".color-button[data-color='yellow'").on('click', function()  {
-    $('.container').append("yellow").addClass('.color-cube')
+    var yellowSquare = $('<div></div>');
+    $('.container').append(yellowSquare);
+    yellowSquare.addClass('color-cube').addClass('yellow');
     $('#yellow').empty();
      clickCountYellow +=1;
     $('#yellow').append('Total yellow: ' + clickCountYellow);
 });
 
 $(".color-button[data-color='green'").on('click', function()  {
-      $(".container").append();
+    var greenSquare = $('<div></div>');
+      $(".container").append(greenSquare);
+      greenSquare.addClass('color-cube').addClass('green');
       $('#green').empty();
        clickCountGreen +=1;
       $('#green').append('Total green: ' + clickCountGreen);
   });
 
 $(".color-button[data-color='blue'").on('click', function()  {
-    $(".container").append();
+    var blueSquare = $('<div></div>');
+    $(".container").append(blueSquare)
+    blueSquare.addClass('color-cube').addClass('blue');
     $('#blue').empty();
     clickCountBlue +=1;
     $('#blue').append('Total blue: ' + clickCountBlue);
